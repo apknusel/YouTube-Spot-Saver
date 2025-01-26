19ebb8cb15a79e506cf88f2b7d0872fc33609b74
Alex Knusel <apknusel@yahoo.com>
2025-01-26 16:15:17 -0600

    Added script for creating zips to release to the chrome web store

---
+++ create_dist.py
@@ -0,0 +1,28 @@
+import zipfile
+import os
+
+# List of files to include in the zip
+files_to_zip = [
+    'background.js',
+    'contentScript.js',
+    'manifest.json',
+    'assets'
+]
+
+# Name of the output zip file
+zip_filename = 'dist.zip'
+
+# Create a ZipFile object
+with zipfile.ZipFile(zip_filename, 'w') as zipf:
+    for file in files_to_zip:
+        if os.path.isdir(file):
+            # If the file is a directory, add all its contents
+            for foldername, subfolders, filenames in os.walk(file):
+                for filename in filenames:
+                    file_path = os.path.join(foldername, filename)
+                    zipf.write(file_path, os.path.relpath(file_path, os.path.join(file, '..')))
+        else:
+            # If the file is a regular file, add it to the zip
+            zipf.write(file)
+
+print(f'Created {zip_filename}')
